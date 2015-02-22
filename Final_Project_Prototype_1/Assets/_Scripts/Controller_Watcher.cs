c5e00825e3e7e60da3402c91838cd839823c2699
Alex Levine <alexml@umich.edu>
2015-02-21 21:34:02 -0500

    Controllers now work though they don't actually control too much at the moment

---
+++ Final_Project_Prototype_1/Assets/_Scripts/Controller_Watcher.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Controller_Watcher : MonoBehaviour {
+    public InputDevice device{get; set;}
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        if(device == null)
+        {
+            return; 
+        }
+
+
+        if (device.GetControl( InputControlType.Action1 ))
+        {
+            rigidbody.AddForce(Vector3.up * 10);
+        }
+        var horiz_movement = device.GetControl( InputControlType.LeftStickX);
+        if (horiz_movement < 0)
+        {
+            rigidbody.AddForce(Vector3.left * 10);
+        }
+        if (horiz_movement > 0)
+        {
+            rigidbody.AddForce(Vector3.right * 10);
+        }
+    }
+}
