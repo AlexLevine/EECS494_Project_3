470353ba1e1f53788425ab06af1878940a640725
Alex Levine <alexml@umich.edu>
2015-02-22 20:29:13 -0500

    We have a level sorta

--- Final_Project_Prototype_1/Assets/_Scripts/Health_gui.cs
+++ Final_Project_Prototype_1/Assets/_Scripts/Health_gui.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class Health_gui : MonoBehaviour {
 
-	Ninja ninja;
-	Llama llama;
-	
-	// Use this for initialization
-	void Start () {
-		ninja = GameObject.Find ("Ninja").GetComponent<Ninja> ();
-		llama = GameObject.Find ("Llama").GetComponent<Llama> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		GUIText gt = this.GetComponent<GUIText> ();
+    Ninja ninja;
+    Llama llama;
+    
+    // Use this for initialization
+    void Start () {
+        ninja = GameObject.Find ("Ninja").GetComponent<Ninja> ();
+        llama = GameObject.Find ("Llama").GetComponent<Llama> ();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        GUIText gt = this.GetComponent<GUIText> ();
 
-		gt.text = "Ninja Health: " + ninja.health + "\nLlama Health: " + llama.health;
+        gt.text = "Ninja Health: " + ninja.health + "\nLlama Health: " + llama.health;
 
-	}
+    }
 }
--- Final_Project_Prototype_1/Assets/_Scripts/Pole.cs
+++ Final_Project_Prototype_1/Assets/_Scripts/Pole.cs
@@ -2,33 +2,33 @@
 using System.Collections;
 
 public class Pole : MonoBehaviour {
-	public bool llama;
-	public bool ninja;
-	public bool level_over;
+    public bool llama;
+    public bool ninja;
+    public bool level_over;
 
-	// Use this for initialization
-	void Start () {
-		llama = false;
-		ninja = false;
-		level_over = false;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if (llama && ninja){
-			renderer.material.color = Color.black;
-			level_over = true;
-		}
-	}
-	
-	void OnTriggerEnter(Collider other){
-		print("ahhh");
-		print (other.name);
-		if (other.name.Contains ("Ninja")){
-			ninja = true;
-		}
-		if (other.name.Contains("Llama")){
-			llama = true;
-		}
-	}
+    // Use this for initialization
+    void Start () {
+        llama = false;
+        ninja = false;
+        level_over = false;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (llama || ninja){
+            renderer.material.color = Color.black;
+            level_over = true;
+        }
+    }
+    
+    void OnTriggerEnter(Collider other){
+        print("ahhh");
+        print (other.name);
+        if (other.name.Contains ("Ninja")){
+            ninja = true;
+        }
+        if (other.name.Contains("Llama")){
+            llama = true;
+        }
+    }
 }
