d8356bbdd91065617a323e5a7b297fefc15f8dcf
James P <jameslp@umich.edu>
2015-02-24 20:48:58 -0500

    made llama spit come out of head. made ninja throwing star vaguely resemble a throwing star

--- Final_Project_Prototype_1/Assets/_Actors/Players/Llama/Llama.cs
+++ Final_Project_Prototype_1/Assets/_Actors/Players/Llama/Llama.cs
@@ -67,10 +67,14 @@ public class Llama : Player_character
 
     public override void projectile_attack()
     {
-        print("thermite!");
+        var projectile_start_pos = transform.position;
+        print(projectile_start_pos);
+        projectile_start_pos += transform.forward * 1f;
+        projectile_start_pos.y += 1f;
+        print(projectile_start_pos);
 
         GameObject spit = Instantiate(
-            spit_prefab, transform.position,
+            spit_prefab, projectile_start_pos,
             transform.rotation) as GameObject;
         spit.rigidbody.velocity = transform.forward * 12;
     }// projectile_attack
