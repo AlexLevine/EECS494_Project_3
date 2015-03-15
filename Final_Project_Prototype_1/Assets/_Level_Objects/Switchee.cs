9cbe83c249ea26c2bca4cd6a16d2b63a551b7ff9
Alex Levine <alexml@umich.edu>
2015-03-15 18:18:37 -0400

    Switches changed a bit to make it work better with my Fire wall

--- Final_Project_Prototype_1/Assets/_Level_Objects/Switchee.cs
+++ Final_Project_Prototype_1/Assets/_Level_Objects/Switchee.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 
 public class Switchee : MonoBehaviour {
-	public bool on;
-	public Material switch_on_material;
-	public Material switch_off_material;
-	// Use this for initialization
-	void Awake () {
-		on = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool on;
+    public Material switch_on_material;
+    public Material switch_off_material;
+    // Use this for initialization
+    void Awake () {
+        on = false;
+    }
+
+    public void toggle()
+    {
+        if(gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);            
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
 }
