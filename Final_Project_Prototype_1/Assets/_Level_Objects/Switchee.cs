aa7d80d66930425df6f036978291fc137f1b63e1
Tyler Friedman <tyfried@umich.edu>
2015-03-15 15:39:00 -0400

    added abstract Switch and Switchee scripts

---
+++ Final_Project_Prototype_1/Assets/_Level_Objects/Switchee.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switchee : MonoBehaviour {
+	public bool on;
+	public Material switch_on_material;
+	public Material switch_off_material;
+	// Use this for initialization
+	void Awake () {
+		on = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
