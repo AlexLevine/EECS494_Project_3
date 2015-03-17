9b3200c677631d5759db90e17b09bc31d4aae2d7
Tyler Friedman <tyfried@umich.edu>
2015-03-16 21:11:08 -0400

    no changes

---
+++ Final_Project_Prototype_1/Assets/_Level_Objects/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+	public Vector3[] checkpoints;
+	public GameObject ll;
+	public GameObject nin;
+	public Vector3 current_ckpt;
+	public int current_index;
+	
+	// Use this for initialization
+	void Start () {
+		current_ckpt = checkpoints[0];
+		current_index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ll.transform.position.z >= current_ckpt.z) current_ckpt = checkpoints[current_index];
+	}
+}
