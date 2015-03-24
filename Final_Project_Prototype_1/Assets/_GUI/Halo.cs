fc84349cc9c326d65ad47a25c7578ff6372b4a28
Tyler Friedman <tyfried@umich.edu>
2015-03-24 19:42:45 -0400

    added lock_on halo

---
+++ Final_Project_Prototype_1/Assets/_GUI/Halo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Halo : MonoBehaviour {
+	public float dist_to_ground;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var sphere = transform.parent.Find ("Sphere");
+		var x = sphere.position.x;
+		var y = sphere.position.y;
+		var z = sphere.position.z;
+		transform.position = new Vector3(x,y-dist_to_ground,z);
+		//print(y);
+	}
+}
