406b9ac417da21cfa0b7b78d6bf23d7edd959a43
James P <jameslp@umich.edu>
2015-03-28 15:18:29 -0400

    system for switching camera viewpoints is in place.

---
+++ Final_Project_Prototype_1/Assets/_Camera/Adjust_camera.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Adjust_camera : MonoBehaviour
+{
+    public float camera_follow_distance;
+    public float camera_hover_height;
+
+    private float camera_y_rotation;
+
+    private bool llama_inside = false;
+    private bool ninja_inside = false;
+
+    private bool is_current_camera_angle = false;
+
+    private List<Adjust_camera> adjustment_points = new List<Adjust_camera>();
+
+    void Awake()
+    {
+        adjustment_points.Add(this);
+    }
+
+    void Start()
+    {
+        camera_y_rotation = transform.rotation.eulerAngles.y;
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject == Llama.get().gameObject)
+        {
+            llama_inside = true;
+
+            if (Llama.get().is_teamed_up)
+            {
+                ninja_inside = true;
+            }
+        }
+
+        if (c.gameObject == Ninja.get().gameObject)
+        {
+            ninja_inside = true;
+        }
+
+        if (llama_inside && ninja_inside && !is_current_camera_angle)
+        {
+            Camera_follow.adjust_main_camera(
+                y_rotation: camera_y_rotation,
+                camera_follow_distance: camera_follow_distance,
+                camera_hover_height: camera_hover_height);
+
+            set_as_current_camera_angle();
+            // is_current_camera_angle = true;
+        }
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        OnTriggerEnter(c);
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject == Llama.get().gameObject)
+        {
+            llama_inside = false;
+        }
+
+        if (c.gameObject == Ninja.get().gameObject)
+        {
+            ninja_inside = false;
+        }
+    }
+
+    // public void notify_not_current_camera_angle()
+    // {
+    //     is_current_camera_angle = false;
+    // }
+
+    public void set_as_current_camera_angle()
+    {
+        foreach (var camera_adj_point in adjustment_points)
+        {
+            camera_adj_point.is_current_camera_angle = false;
+        }
+
+        is_current_camera_angle = true;
+    }// set_as_current_camera_angle
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+}
--- Final_Project_Prototype_1/Assets/_GUI/Change_to_arena_cam.cs
+++ Final_Project_Prototype_1/Assets/_GUI/Change_to_arena_cam.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Change_to_arena_cam : MonoBehaviour
 {
+    public GameObject arena_center;
 
     void OnTriggerEnter(Collider other)
     {
@@ -13,6 +14,13 @@ public class Change_to_arena_cam : MonoBehaviour
 
         // Camera_follow.in_boss_arena = true;
         Samurai_Attack.get().notify_players_in_arena();
+
+        // Camera.main.GetComponent<Camera_follow>().enabled = false;
+        // var arena_camera_script = Camera.main.GetComponent<Arena_camera>();
+        // arena_camera_script.arena_center = arena_center;
+        // arena_camera_script.enabled = true;
+
+        Destroy(gameObject);
     }
 
     void OnTriggerStay(Collider other)
