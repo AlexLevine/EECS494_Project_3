fa806a19d25fe9155cbb84ce0f4b3a7df6da6389
Alex Levine <alexml@umich.edu>
2015-03-28 15:47:17 -0400

    more gui

--- Final_Project_Prototype_1/Assets/_GUI/Timer.cs
+++ Final_Project_Prototype_1/Assets/_GUI/Timer.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float cur_time;
     static public int num_enemies_killed_by_llama;
     static public int num_enemies_killed_by_ninja;
+
+    public Text llama_txt, ninja_txt; 
 
     void Start()
     {
@@ -17,6 +20,9 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
+
+        llama_txt.text = "Score: " + num_enemies_killed_by_llama.ToString();
+        ninja_txt.text = "Score: " + num_enemies_killed_by_ninja.ToString();
         cur_time += Time.deltaTime;
     }
 
@@ -24,7 +30,7 @@ public class Timer : MonoBehaviour
     void OnGUI()
     {
         // GUI.Box(new Rect(0, Screen.height - 25, 120, 25), "Time: " + cur_time.ToString().Substring(0,4));
-        GUI.Box(new Rect(0, Screen.height - 50, 120, 25), "Llama Score: " + num_enemies_killed_by_llama.ToString());
-        GUI.Box(new Rect(0, Screen.height - 75, 120, 25), "Ninja Score: " + num_enemies_killed_by_ninja.ToString());
+        // GUI.Box(new Rect(0, Screen.height - 50, 120, 25), );
+        // GUI.Box(new Rect(0, Screen.height - 75, 120, 25), 
     }
 }
