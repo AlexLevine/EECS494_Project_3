66fd4a4b4ca1bb3befc04125b3b63f6267f0cdd4
mbreakey <mbreakey@umich.edu>
2015-03-28 17:53:14 -0400

    added stationary enemy

---
+++ Final_Project_Prototype_1/Assets/_Actors/Enemies/Stationary_enemy/projectile2_script.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectile2_script : Enemy {
+
+	public float speed = 7f;
+	public Vector3 angle = new Vector3(1, 0, 0);
+	private Vector3 knockback = new Vector3(0, 0, 0);
+	
+	public override int attack_power
+	{
+		get
+		{
+			return 1;
+		}
+	}
+	
+	public override int max_health
+	{
+		get
+		{
+			return 1;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().velocity = angle * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision c)
+	{
+		
+		if (c.gameObject.tag == "Player")
+		{
+			c.gameObject.GetComponent<Actor>().receive_hit(attack_power, knockback, this.gameObject);
+		}
+		Destroy (gameObject);
+	}// OnCollisionEnter
+	
+	
+}
