0f860d2346a158fe52326554c5fb9e686c8240cf
Tyler Friedman <tyfried@umich.edu>
2015-03-29 23:39:21 -0400

    added room with bridge that activates when all enemies are killed

---
+++ Final_Project_Prototype_1/Assets/_Level_Objects/Enemy_Counter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy_Counter : MonoBehaviour {
+	public GameObject switchee;
+	//public int enemy_count;
+	private List<Collider> enemies;
+	// Use this for initialization
+	void Start () {
+		enemies = new List<Collider>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		foreach(Collider g in enemies){
+			if (g!=null) return;
+		}
+		Switchee sw = switchee.GetComponent<Switchee>();
+		sw.activate();
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.name.Contains("enemy")){
+			enemies.Add (other);
+		}
+	}
+	
+}
