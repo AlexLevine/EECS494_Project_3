d2735359d0e5b94ce4e46d54648e47b2046fc652
James P <jameslp@umich.edu>
2015-03-30 15:17:13 -0400

    charge steering works

--- Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
+++ Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 3,
-                Build = 4680
+                Build = 4703
             };
         }
 
