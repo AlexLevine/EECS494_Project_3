b6d5a8b613e75511124d266b539329a6dbc99884
James P <jameslp@umich.edu>
2015-03-30 20:12:11 -0400

    wrote sound effect randomizer script

---
+++ Final_Project_Prototype_1/Assets/Sound_effect_randomizer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Sound_effect_randomizer : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void play()
+    {
+        if (clips.Length == 0)
+        {
+            print("no clips");
+            return;
+        }
+
+        source.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+    }
+
+}
