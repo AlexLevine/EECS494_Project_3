04618086e0aa67d13d383ef663b92343c410a07a
James P <jameslp@umich.edu>
2015-03-30 21:07:16 -0400

    fixed health bar lock on thing

--- Final_Project_Prototype_1/Assets/_GUI/Lock_on_health_bar.cs
+++ Final_Project_Prototype_1/Assets/_GUI/Lock_on_health_bar.cs
@@ -8,20 +8,20 @@ public class Lock_on_health_bar : MonoBehaviour {
 
     void Start()
     {
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update () 
+    void Update ()
     {
         if(target == null)
         {
             gameObject.SetActive(false);
-            return; 
+            return;
         }
 
-        update_position(); 
-        update_slider(); 
+        update_position();
+        update_slider();
 
     }
 
@@ -30,18 +30,18 @@ public class Lock_on_health_bar : MonoBehaviour {
     {
         Vector3 screen_point = Camera.main.WorldToViewportPoint(target.transform.position);
 
-        float x_pos = screen_point.x * Screen.width; 
-        float y_pos = (screen_point.y * Screen.height) - 20; 
+        float x_pos = screen_point.x * Screen.width;
+        float y_pos = (screen_point.y * Screen.height) - 20;
 
         transform.position = new Vector3(x_pos, y_pos, 0);
     }
 
 
-    void update_slider() 
+    void update_slider()
     {
         float health_percentage = (float)target.GetComponent<Actor>().health /
-                                  (float)target.GetComponent<Actor>().max_health; 
+                                  (float)target.GetComponent<Actor>().max_health;
 
-        this.GetComponent<Slider>().normalizedValue = health_percentage; 
+        this.GetComponent<Slider>().normalizedValue = health_percentage;
     }
 }
