d57d4e19a4b860f06039dc84ee29644600eac6f5
mbreakey <mbreakey@umich.edu>
2015-04-11 16:48:20 -0400

    sword

--- Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
+++ Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 3,
-                Build = 5226
+                Build = 5243
             };
         }
 
