bc0970591988b26b27ff84d0526a8e82d49464f0
James P <jameslp@umich.edu>
2015-04-15 03:16:20 -0400

    cut scene bugfix

--- Final_Project_Prototype_1/Assets/_Camera/Cut_scene.cs
+++ Final_Project_Prototype_1/Assets/_Camera/Cut_scene.cs
@@ -25,8 +25,8 @@ public class Cut_scene : MonoBehaviour
 
 	public void unpause_all()
 	{
-		Camera_follow.stop_following_player();
+		Camera_follow.start_following_player();
 		Input_reader.toggle_player_controls();
-		Actor.actors_paused = true;
+		Actor.actors_paused = false;
 	}// unpause_all
 }
