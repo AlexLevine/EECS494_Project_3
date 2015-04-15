2ec1a2d0e331946e635f2d0db24fdaa1ecbb1631
Tyler Friedman <tyfried@umich.edu>
2015-04-14 23:14:00 -0400

    icon thing is stupid and buggy, i have a temporary fix, once we choose a resolution then I can hardcode the values

--- Final_Project_Prototype_1/Assets/_GUI/Icon.js
+++ Final_Project_Prototype_1/Assets/_GUI/Icon.js
@@ -1,13 +1,32 @@
 ﻿#pragma strict
 
 public var texture: Texture;
+public var frame: float;
+
+var center: Vector3;
+var base_vector: Vector3;
+var upper_right: float;
+var upper_left: float;
+var lower_left: float;
+var lower_right: float;
+
+public var icon_ratio_y = .77f;
+public var icon_ratio_x = .95f;
+public var scale = .01f;
 
 function Start () {
-
+	center = new Vector3(Screen.width/2f,Screen.height/2f,0);
+	base_vector = new Vector3(Camera.main.pixelWidth,Camera.main.pixelHeight/2f,0) - center;
+	upper_right = angle(base_vector,new Vector3(Camera.main.pixelWidth,Camera.main.pixelHeight,0) - center);
+	upper_left = angle(base_vector, new Vector3(0,Camera.main.pixelHeight,0)-center);
+	lower_left = angle(base_vector, new Vector3(0,0,0)-center);
+	lower_right = angle(base_vector,new Vector3(Camera.main.pixelWidth,0,0)-center);
+	
 }
 
 function Update () {
-
+	//print(Camera.main.pixelWidth);
+	//print(Camera.main.pixelHeight);
 }
 
  //Two utility functions to find the intersection points between 2D lines and rectangles
@@ -83,6 +102,53 @@ function Update () {
          ap = Camera.main.ViewportToScreenPoint(ap);
          ap.y = Screen.height - ap.y;
          
-         GUI.DrawTexture(Rect(ap.x - 75, ap.y - 75, 75, 75),texture);
+         if (ap.x==0) ap.x=ap.x+frame;
+         else if (ap.y==Screen.height) ap.y=ap.y-frame;
+         else if (ap.x==Screen.width) ap.x=ap.x-frame;
+         
+         
+         
+//		var ang = angle(base_vector,Vector3(ap.x,ap.y,0)-center);
+//	
+//		if (ang<=upper_right && ang>lower_right){
+//			print ("right");
+//			ap.x = icon_ratio_x*Camera.main.pixelWidth;
+//		}
+//		else if (ang<=upper_left && ang>upper_right){
+//			print ("top");
+//			ap.y = (1-icon_ratio_y)*Camera.main.pixelHeight;
+//		}
+//		else if (ang<=lower_left || ang>upper_left){ //due to atan2 implementation
+//			print ("left");
+//			ap.x = (1-icon_ratio_x)*Camera.main.pixelWidth;
+//		}
+//		else if (ang<=lower_right && ang>lower_left){
+//			print ("bottom");
+//			ap.y = (icon_ratio_y)*Camera.main.pixelHeight;
+//		}
+
+//		if (ap.x>=icon_ratio_x*Screen.width) ap.x = icon_ratio_x*Screen.width;
+//		if (ap.x<=(1-icon_ratio_x)*Screen.width) ap.x = (1-icon_ratio_x)*Screen.width;
+//        if (ap.y>=icon_ratio_y*Screen.height) ap.y = icon_ratio_y*Screen.height;
+//		if (ap.y<=(1-icon_ratio_y)*Screen.height) ap.y = (1-icon_ratio_y)*Screen.height;
+        
+        var current = Vector3(ap.x,ap.y,0);
+        var target = Vector3(Screen.width/2,Screen.height/2,0);
+        
+        var p = Vector3.MoveTowards(current,target,20f);
+        ap.x = p.x; ap.y = p.y;
+        			
+        print(ap);
+         GUI.DrawTexture(Rect(ap.x, ap.y, 40, 40),texture);
+         
+         print(Screen.width*Screen.height*scale);
+         //GUI.Box(Rect(ap.x,ap.y,5,5),".");
      }
+     
+ }
+ 
+ function angle(b_v: Vector3, arm: Vector3){
+		var dot_product = b_v.x*arm.x + b_v.y*arm.y;
+		var determinant = b_v.x*arm.y - b_v.y*arm.x;
+		return Mathf.Atan2(determinant,dot_product);
  }
