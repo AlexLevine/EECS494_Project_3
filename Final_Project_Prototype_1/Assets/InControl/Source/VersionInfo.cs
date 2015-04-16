efe2b8cbe1640530753874a660cf1f519ccf5d98
James P <jameslp@umich.edu>
2015-04-15 21:06:05 -0400

    tweaked camera angle for shrink puzzle.

--- Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
+++ Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 3,
-                Build = 5707
+                Build = 5717
             };
         }
 
