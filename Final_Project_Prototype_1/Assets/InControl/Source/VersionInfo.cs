9dd7db6a314f3e9a4a5082372215ea5b51d22fca
Alex Levine <alexml@umich.edu>
2015-04-18 23:21:58 -0400

    Things are work

--- Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
+++ Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 3,
-                Build = 6118
+                Build = 6132
             };
         }
 
