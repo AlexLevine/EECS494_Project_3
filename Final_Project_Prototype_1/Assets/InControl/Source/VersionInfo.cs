da7edc1278b82e779708edfbe35c89cf2dfead17
Alex Levine <alexml@umich.edu>
2015-04-18 23:04:05 -0400

    Stationary Enemy dumb garbage stuff

--- Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
+++ Final_Project_Prototype_1/Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 3,
-                Build = 6080
+                Build = 6118
             };
         }
 
