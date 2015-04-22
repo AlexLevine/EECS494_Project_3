5b93109ca2cb2e30f4fa74d8c3eae0bc80d788a8
Alex Levine <alexml@umich.edu>
2015-04-22 00:20:53 -0400

    Working on diamonds

---
+++ Final_Project_Prototype_1/Assets/Diamond_Manager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Diamond_Manager : Switchee {
+
+    public bool running = false; 
+    public float speed = 2f; 
+    public GameObject target_position; 
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        if(!running)
+        {
+            return; 
+        }
+
+        lerp_downwards(); 
+        rotate_around_self(); 
+
+    }
+
+    public override void activate(
+        Switchee_callback callback, float? callback_delay)
+    {
+        base.activate(callback, callback_delay); 
+
+        running = true; 
+    }
+
+    private void lerp_downwards()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+            target_position.transform.position, Time.deltaTime);
+    }
+
+    private void rotate_around_self()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 20f);
+    }
+
+    IEnumerator
+}
