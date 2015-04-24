80a0d6d3c7e1da49d0255f8573bcdaac4d004880
James P <jameslp@umich.edu>
2015-04-24 01:29:01 -0400

    fixed title screen null reference stuff

--- Final_Project_Prototype_1/Assets/_Actors/Shared/Title_screen_input_reader.cs
+++ Final_Project_Prototype_1/Assets/_Actors/Shared/Title_screen_input_reader.cs
@@ -5,56 +5,56 @@ using InControl;
 
 public class Title_screen_input_reader : MonoBehaviour
 {
-    public int player_id = 0;
+    // public int player_id = 0;
 
-    public InputDevice input_device = null;
+    // public InputDevice input_device = null;
 
-    public GameObject title_controller; 
+    // public GameObject title_controller;
 
-    // Use this for initialization
-    void Start()
-    {
-        get_input_device();
+    // // Use this for initialization
+    // void Start()
+    // {
+    //     get_input_device();
 
-    }
+    // }
 
-    //--------------------------------------------------------------------------
+    // //--------------------------------------------------------------------------
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (input_device == null)
-        {
-            get_input_device(); 
-        }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (input_device == null)
+    //     {
+    //         get_input_device();
+    //     }
 
-        if (input_device.GetControl(InputControlType.Action1).WasPressed)
-        {
-            if(player_id == 0)
-            {
-                print("0");
-                title_controller.GetComponent<Title_screen_controller>().setLlama(); 
-            }
+    //     if (input_device.GetControl(InputControlType.Action1).WasPressed)
+    //     {
+    //         if(player_id == 0)
+    //         {
+    //             print("0");
+    //             title_controller.GetComponent<Title_screen_controller>().setLlama();
+    //         }
 
-            if(player_id == 1)
-            {
-                print("1");
-                title_controller.GetComponent<Title_screen_controller>().setNinja(); 
-            }
-    
-            Destroy(gameObject);
-        }
-    }
+    //         if(player_id == 1)
+    //         {
+    //             print("1");
+    //             title_controller.GetComponent<Title_screen_controller>().setNinja();
+    //         }
 
-    void get_input_device()
-    {
-        if(player_id == 0)
-        {
-            input_device = Llama.get().GetComponent<Input_reader>().input_device;
-        }
-        else
-        {
-            input_device = Ninja.get().GetComponent<Input_reader>().input_device;   
-        }
-    }
+    //         Destroy(gameObject);
+    //     }
+    // }
+
+    // // void get_input_device()
+    // // {
+    // //     if(player_id == 0)
+    // //     {
+    // //         input_device = Llama.get().GetComponent<Input_reader>().input_device;
+    // //     }
+    // //     else
+    // //     {
+    // //         input_device = Ninja.get().GetComponent<Input_reader>().input_device;
+    // //     }
+    // // }
 }
